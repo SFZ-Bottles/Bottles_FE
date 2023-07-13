8389cf573708a3de738de55344317aadcfb8bfc7
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-07-13 21:57:55 +0900

    [Chore] Recoil 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import LogInPage from './pages/LogInPage';
+import HomePage from './pages/Homepage/HomePage';
+import { RecoilRoot } from 'recoil';
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LogInPage/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LogInPage/>}/>
+          <Route path='/Home' element={<HomePage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 

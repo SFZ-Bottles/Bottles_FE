c1f4fedbe8800b634ff81f498f9a4285d62ec88b
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-07-25 23:07:11 +0900

    [Style] modify style

--- src/pages/LoginPage/LogInPage.tsx
+++ src/pages/LoginPage/LogInPage.tsx
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { Form, LogInDiv, Input, Title, LogInButton, UnderLine, InvalidBox, C_ColFlexBox } from "../../styled-components/styled_LogIn"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { signIn } from "../../services/API";
+import { checkToken } from "../../context/function";
 
 function LogInPage() {
     const navigate = useNavigate();
@@ -22,6 +23,10 @@ function LogInPage() {
             setIdentity((prev: boolean) => !prev);
         }
     }
+
+    useEffect(() => {
+        checkToken({navigate});
+    },[]);
 
     return(
         <LogInDiv>

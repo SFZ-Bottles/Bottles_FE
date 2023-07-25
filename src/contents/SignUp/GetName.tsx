5162dcab02e11732f5aa400196a87226f0ecf0ec
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-07-25 23:04:20 +0900

    [Fix] fix API call error

--- src/contents/SignUp/GetName.tsx
+++ src/contents/SignUp/GetName.tsx
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
-import { SignupState, signupPage } from "../../Atom/atom";
+import { SignupState, signupPage } from "../../atom/atom";
 import { AvailableBox, C_FlexBox, CheckId, FlexBox, Form, IdLength, Input, InputDiv, PasswordLength, SignInDiv, LoginInfo, SemiTitle, Span, Title, NextButton } from "../../styled-components/styled_LogIn";
 import { useRecoilState } from 'recoil';
 import { checkDuplicate } from "../../services/API";
 import { useNavigate } from "react-router-dom";
+import { isValidInput } from "../../context/function";
 
 function GetName() {
     const [signup,setSignup] = useRecoilState(SignupState);
     const [pageNum,setPageNum] = useRecoilState(signupPage);
 
+    const onSubmit = async () => {
+        if(isValidInput(signup.name.length,30) && isValidInput(signup.email.length, 30)){
+            setPageNum((prev) => prev + 1);
+        }
+    }
     return(
         <SignInDiv>
             <SemiTitle>
                 Welcome to Bottles!
             </SemiTitle>
-            <Form>
+            <Form onSubmit={onSubmit}>
                 <InputDiv>
                     <Span>Name
                         <LoginInfo>이름 정보는 프로필 상단에 표시되며 이를 통해 친구와 연결될 수 있습니다.</LoginInfo>
@@ -23,20 +29,20 @@ function GetName() {
                         <Input type="text" placeholder="Name" value={signup.name} onChange={(e: any) => setSignup({...signup, name: e.target.value})} required/>
                     </C_FlexBox>
                     <PasswordLength
-                    length={signup?.id.length}
+                    len={signup?.name.length}
                     >{signup?.name.length} / 30</PasswordLength>
                 </InputDiv>
                 <InputDiv>
                     <Span>E-mail
                         <LoginInfo>이메일 정보는 계정정보 분실시 이용됩니다. 정확한 주소를 입력해 주세요.</LoginInfo>
                     </Span>
-                    <Input type="password" placeholder="E-mail" value={signup.email} onChange={(e: any) => setSignup({...signup, email: e.target.value})} required/>
+                    <Input type="text" placeholder="E-mail" value={signup.email} onChange={(e: any) => setSignup({...signup, email: e.target.value})} required/>
                     <PasswordLength
-                    length={signup?.password.length}
+                    len={signup?.password.length}
                     >{signup?.email.length} / 30</PasswordLength>
                 </InputDiv>
+                <NextButton>({pageNum} / 3) 다음단계로</NextButton>
             </Form>
-            <NextButton onClick={() => setPageNum(pageNum + 1)}>({pageNum} / 3) 다음단계로</NextButton>
         </SignInDiv>
     )
 }

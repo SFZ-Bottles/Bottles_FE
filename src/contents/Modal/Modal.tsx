98cc26ebb71a7fd50404b6a44238ab080a17ba76
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-08-09 14:37:41 +0900

    [Feat] overlay 클릭시 닫힘 구현

--- src/contents/Modal/Modal.tsx
+++ src/contents/Modal/Modal.tsx
@@ -10,26 +10,38 @@ const ModalOverlay = styled.div`
   background-color: rgba(0, 0, 0, 0.3);
 `;
 
-const ModalContent = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #ffffff;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  z-index: 2;
-`;
-
 const Modal = ({ onClose, children }: any) => {
   return (
-    <ModalOverlay onClick={onClose}>
-      <ModalContent onClick={(e) => e.stopPropagation()}>
+    <S.ModalOverlay onClick={onClose}>
+      <S.ModalContent onClick={(e) => e.stopPropagation()}>
         {children}
-      </ModalContent>
-    </ModalOverlay>
+      </S.ModalContent>
+    </S.ModalOverlay>
   );
 };
 
+const S = {
+  ModalContent: styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #ffffff;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    z-index: 2;
+  `,
+  ModalOverlay: styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
+`
+};
+
+
 export default Modal;
+

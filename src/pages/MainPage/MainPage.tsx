b6e1adc16ab963cca459ac26b44c9d313f9ddf57
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-08-16 23:06:13 +0900

    [Rename] merge 후 rename

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,17 +1,31 @@
 
-import React from 'react';
-import { HeaderContainer, HeaderItem, LogoutItem, PageContainer, ImageContainer } from "../../styled-components/styled_Main"
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../../contents/Navbar/Navbar';
+import { styled } from 'styled-components';
 
 const MainPage = () => {
   const location = useLocation();
   return (
-    <>
+    <S.MainContainer>
       <Navbar/>
-      <Outlet/>
-    </>
+      <S.OutletConatiner>
+        <Outlet/>
+      </S.OutletConatiner>
+    </S.MainContainer>
   );
 };
+
+const S = {
+  MainContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    min-height: 80vh;
+  `,
+  OutletConatiner: styled.div`
+    padding-top: 100px;
+  `
+}
 
 export default MainPage;

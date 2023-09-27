ee4fcebecdfceb2e65494deb597b3ed67ec80d15
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-09-27 19:45:51 +0900

    [Fix] props 대문자 포함시 에러 발생 수정

--- src/contents/Navbar/Navbar.tsx
+++ src/contents/Navbar/Navbar.tsx
@@ -2,25 +2,39 @@ import { Link, useLocation } from "react-router-dom";
 import { HeaderContainer, HeaderItem, LogoutItem } from "../../styled-components/styled_Main";
 import { themeState } from "../../Atom/atom";
 import { useRecoilState, useSetRecoilState } from "recoil";
+import { styled } from "styled-components";
 
 function Navbar() {
     const location = useLocation();
     const setTheme = useSetRecoilState(themeState);
 
     const titleClick = () => {
-        setTheme((prev:boolean) => !prev);
     };
+        setTheme((prev:boolean) => !prev);
     return(
         <HeaderContainer>
-            <HeaderItem isTitle onClick={titleClick}>BOTTLES</HeaderItem>
-            <HeaderItem isActive={location.pathname === "/home/feed"}><Link to="/home/feed">Home</Link></HeaderItem>
-            <HeaderItem isActive={location.pathname === "/home/search"}><Link to="/home/search">Search</Link></HeaderItem>
-            <HeaderItem isActive={location.pathname === "/home/message"}><Link to="/home/message">Message</Link></HeaderItem>
-            <HeaderItem isActive={location.pathname === "/home/myalbums"}><Link to="/home/myalbums">My Albums</Link></HeaderItem>
-            <HeaderItem isActive={location.pathname === "/home/setting"}><Link to="/home/setting">Setting</Link></HeaderItem>
+            <HeaderItem title={true} active={false} onClick={titleClick}>BOTTLES</HeaderItem>
+            <HeaderItem title={false} active={location.pathname === "/home/feed"}><S.StyledLink to="/home/feed">Home</S.StyledLink></HeaderItem>
+            <HeaderItem title={false} active={location.pathname === "/home/search"}><S.StyledLink to="/home/search">Search</S.StyledLink></HeaderItem>
+            <HeaderItem title={false} active={location.pathname === "/home/message"}><S.StyledLink to="/home/message">Message</S.StyledLink></HeaderItem>
+            <HeaderItem title={false} active={location.pathname === "/home/myalbums"}><S.StyledLink to="/home/myalbums">My Albums</S.StyledLink></HeaderItem>
+            <HeaderItem title={false} active={location.pathname === "/home/setting"}><S.StyledLink to="/home/setting">Setting</S.StyledLink></HeaderItem>
             <LogoutItem>Logout</LogoutItem>
       </HeaderContainer>
     );
+}
+
+const S = {
+    StyledLink: styled(Link)`
+        text-decoration: none;
+        color: black;
+        color: ${props => props.theme.color.fontColor};
+        text-decoration: none; 
+        transition: font-weight 0.2s; 
+        &:hover {
+        font-weight: bold;
+        }
+    `
 }
 
 export default Navbar;

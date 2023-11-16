9a60f547ec5065a5558900039c4370d81b3b6cec
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-11-16 15:44:17 +0900

    refactor: FeedPage가 MyAlbum 페이지에서도 보일 수 있도록 컴포넌트화

--- src/pages/FeedPage/FeedPage.tsx
+++ src/pages/FeedPage/FeedPage.tsx
@@ -5,67 +5,68 @@ import Comment from "../../contents/Comment/CommentModal";
 import Modal2 from "../../contents/Modal/Modal2";
 import CommentImage from "../../contents/Comment/CommentImage";
 import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
 
-function FeedPage(){
-    const id: string | null= localStorage.getItem('id');
-    const [modal, setModal] = useState(false);
-    const [AlbumId, setAlbumId] = useState<string>();
-    const {isLoading, data} = useQuery(["feedAlbum",id], getAlbum);
-    
-    const onImgClick = (id: string) => {
-        setModal(true);
-        setAlbumId(id);
-    };
+function FeedPage() {
+  const params = useParams();
+  const { id } = params;
+  const [modal, setModal] = useState(false);
+  const [AlbumId, setAlbumId] = useState<string>();
+  const { isLoading, data } = useQuery(["feedAlbum", id ?? "follow"], () =>
+    getAlbum(id ?? "follow")
+  );
 
-    return(
-        <S.Container>
-            <S.AlbumContainer>
-                {data?.map((album: any, index: number) => (
-                <S.ImgDiv 
-                key={index}
-                onClick={() => onImgClick(album.id)} 
-                address={album.cover_image_url}>
-                </S.ImgDiv>
-                )
-                )}
-            </S.AlbumContainer>
-            {modal && AlbumId? 
-                <div style={{display: "flex", position: "absolute"}}>
-                <Modal2 onClose={() => setModal(false)}>
-                    {{
-                        left: <CommentImage AlbumId={AlbumId}/>,
-                        right: <Comment AlbumId={AlbumId}/>
-                    }}
-                </Modal2>
-                </div>
-            :
-            null
-            }
-        </S.Container>
-    )
+  const onImgClick = (id: string) => {
+    setModal(true);
+    setAlbumId(id);
+  };
+
+  return (
+    <S.Container>
+      <S.AlbumContainer>
+        {data?.map((album: any, index: number) => (
+          <S.ImgDiv
+            key={index}
+            onClick={() => onImgClick(album.id)}
+            address={album.cover_image_url}
+          ></S.ImgDiv>
+        ))}
+      </S.AlbumContainer>
+      {modal && AlbumId ? (
+        <div style={{ display: "flex", position: "absolute" }}>
+          <Modal2 onClose={() => setModal(false)}>
+            {{
+              left: <CommentImage AlbumId={AlbumId} />,
+              right: <Comment AlbumId={AlbumId} />,
+            }}
+          </Modal2>
+        </div>
+      ) : null}
+    </S.Container>
+  );
 }
 
 const S = {
-    Container: styled.div`
-        display: flex;
-        width: 100%;
-        height: 100%;
-        justify-content: center;
-    `,
-    AlbumContainer: styled.div`
-        display: grid;
-        padding-top: 40px;
-        grid-template-columns: repeat(2,1fr);
-        width: 800px;
-        gap: 10px;
-        height: 100%;
-    `,
-    ImgDiv: styled.div<{address: string}>`
-        background-image: url(${props=> props.address});
-        width: 400px;
-        height: 400px;
-        background-size: cover;
-    `
-}
+  Container: styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+  `,
+  AlbumContainer: styled.div`
+    display: grid;
+    padding-top: 40px;
+    grid-template-columns: repeat(2, 1fr);
+    width: 800px;
+    gap: 10px;
+    height: 100%;
+  `,
+  ImgDiv: styled.div<{ address: string }>`
+    background-image: url(${(props) => props.address});
+    width: 400px;
+    height: 400px;
+    background-size: cover;
+  `,
+};
 
 export default FeedPage;

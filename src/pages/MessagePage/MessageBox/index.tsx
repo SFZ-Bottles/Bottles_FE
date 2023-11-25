56a67691601244f1c794ec27d201b85fdcaff2a7
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-11-25 19:42:11 +0900

    refactor: Message 페이지 컴포넌트 모듈화 작업 수행

---
+++ src/pages/MessagePage/MessageBox/index.tsx
@@ -0,0 +1,42 @@
+import { styled } from "styled-components";
+import { ProfileProps } from "../MessagePage";
+
+interface MessageBoxProps {
+  userInfo: ProfileProps[] | undefined;
+  clickIndex: number;
+}
+
+function MessageBox({ userInfo, clickIndex }: MessageBoxProps) {
+  return (
+    <S.ChatProfileDiv>
+      {userInfo && <S.Profile src={userInfo[clickIndex].image || ""} />}
+      {userInfo && <S.UserName>{userInfo[clickIndex].members[1]}</S.UserName>}
+    </S.ChatProfileDiv>
+  );
+}
+
+const S = {
+  UserName: styled.div`
+    font-size: 1.5rem;
+    font-weight: 700;
+  `,
+
+  Profile: styled.img<{ src: string }>`
+    width: 70px;
+    height: 70px;
+    border: 2px solid #d9d9d9;
+    border-radius: 40px;
+    background-image: url(src);
+  `,
+
+  ChatProfileDiv: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 160px;
+  `,
+};
+
+export default MessageBox;
---
+++ src/pages/MessagePage/UserCard/index.tsx
@@ -0,0 +1,54 @@
+import { styled } from "styled-components";
+import { Card } from "../../../contents/Comment/Comment";
+import { ProfileProps } from "../MessagePage";
+import { useEffect } from "react";
+
+interface UserCardProps {
+  data: ProfileProps[] | undefined;
+  setClickIndex: React.Dispatch<React.SetStateAction<number>>;
+}
+
+function UserCard({ data, setClickIndex }: UserCardProps) {
+  console.log(data);
+  return (
+    <S.ItemContainer>
+      {data?.map((info: ProfileProps, index: number) => (
+        <S.Item key={index}>
+          <Card onClick={() => setClickIndex(index)}>
+            {info.image && <Card.UserProfile src={info.image} />}
+            <Card.UserId>{info?.members[1]}</Card.UserId>
+            <Card.UserDescribe>{info.name}</Card.UserDescribe>
+            <Card.MessageImg />
+          </Card>
+        </S.Item>
+      ))}
+    </S.ItemContainer>
+  );
+}
+
+const S = {
+  ItemContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    width: 450px;
+    height: 100%;
+    overflow: auto;
+  `,
+
+  Item: styled.div`
+    display: flex;
+    align-items: center;
+    width: 370px;
+    height: 100px;
+    border: 2px solid #d9d9d9;
+    border-radius: 2rem;
+    justify-content: space-between;
+    padding: 0 20px;
+    cursor: pointer;
+  `,
+};
+
+export default UserCard;

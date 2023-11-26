afb4657bb925256a15ee8090204df37b5daeb46b
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-11-27 01:01:31 +0900

    style: 전역에서 사용 가능한 common style 정의

---
+++ src/styled-components/commonStyle.ts
@@ -0,0 +1,20 @@
+import { css } from "styled-components";
+
+export const FlexCenterCSS = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FlexColumnCenterCss = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const WidthLimitCSS = css`
+  width: 95%;
+  max-width: 119.4rem;
+  margin: 0 auto;
+`;

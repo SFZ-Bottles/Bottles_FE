f2a6f512f7595e6388f543c60bb17639146df895
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-11-28 21:54:43 +0900

    refactor: Input 공통 컴포넌트 생성 및 적용

--- src/contents/SearchBar/SearchBar.tsx
+++ src/contents/SearchBar/SearchBar.tsx
@@ -1,61 +1,67 @@
 import { useEffect } from "react";
 import { styled } from "styled-components";
+import CommonInput from "../Input/Input";
 
-interface IProps{
-    input: string;
-    setInput: React.Dispatch<React.SetStateAction<string>>;
+interface IProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const SearchBar = ({input,setInput}: IProps) => {
-  
+const SearchBar = ({ value, onChange, ...rest }: IProps) => {
   return (
-      <>
-        <S.SearchContainer>
-          <S.SearchDiv>
-            <S.InputDiv value={input} onChange={(e) => setInput(e.target.value)}/>
-            <S.SearchIcon/>
-          </S.SearchDiv>
-        </S.SearchContainer>
-      </>
-    );
-  };
-  
-  const S = {
-    SearchContainer: styled.div`
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 100%;
-      height: 100px;
-      background-color: ${props => props.theme.color.bgColor};
-    `,
-  
-    SearchDiv: styled.div`
-      display: flex;
-      align-items: center;
-      width: 40%;
-      position: relative;
-    `,
-  
-    SearchIcon: styled.div`
-      display: flex;
-      position: absolute;
-      width: 20px;
-      height: 20px;
-      margin-left: 10px;
-      background-size: cover;
-      background-image: url('/img/search.svg');
-    `,
-  
-    InputDiv: styled.input`
-      display: flex;
-      width: 100%;
-      height: 40px;
-      border: none;
-      background-color: #E8E8E8;
-      border-radius: 1rem;
-      padding-left: 40px;
-    `,
-  }
-  
-  export default SearchBar;
+    <>
+      <S.SearchContainer>
+        <S.SearchDiv>
+          <CommonInput
+            type="text"
+            value={value}
+            name={value}
+            onChange={onChange}
+            {...rest}
+          />
+          <S.SearchIcon />
+        </S.SearchDiv>
+      </S.SearchContainer>
+    </>
+  );
+};
+
+const S = {
+  SearchContainer: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100px;
+    background-color: ${(props) => props.theme.color.bgColor};
+  `,
+
+  SearchDiv: styled.div`
+    display: flex;
+    align-items: center;
+    width: 40%;
+    position: relative;
+  `,
+
+  SearchIcon: styled.div`
+    display: flex;
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    margin-left: 10px;
+    background-size: cover;
+    background-image: url("/img/search.svg");
+  `,
+
+  InputDiv: styled.input`
+    display: flex;
+    width: 100%;
+    height: 40px;
+    border: none;
+    background-color: #e8e8e8;
+    border-radius: 1rem;
+    padding-left: 40px;
+  `,
+};
+
+export default SearchBar;

8bc3949ccd7b739756dc6b337244789fbedc9c8b
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-12-03 13:03:44 +0900

    refactor: 공통 컴포넌트 Input 사용

--- src/contents/Modal/EditModal.tsx
+++ src/contents/Modal/EditModal.tsx
@@ -1,6 +1,11 @@
 import { styled } from "styled-components";
 import { changeInfo } from "../../services/API";
 import { useState } from "react";
+import {
+  FlexCenterCSS,
+  FlexColumnCenterCss,
+} from "../../styled-components/commonStyle";
+import CommonInput from "../Input/Input";
 
 export interface IEdit {
   id: string;
@@ -13,51 +18,47 @@ export interface IEdit {
 function EditModal({ editData, userData, setUserData, onClose }: any) {
   const [inputData, setInputData] = useState("");
   const onClick = async (editData: string) => {
-    setUserData({ ...userData, [editData]: inputData });
-    console.log({ ...userData, [editData]: inputData });
-    await changeInfo({ ...userData, [editData]: inputData });
-    onClose(null);
+    try {
+      await changeInfo({ ...userData, [editData]: inputData });
+      setUserData({ ...userData, [editData]: inputData });
+      onClose(null);
+    } catch (error: any) {
+      alert(error.message);
+    }
   };
 
   return (
     <S.Container>
       {editData}
-      <S.Input
+      <CommonInput
+        type="text"
+        name="inputData"
         value={inputData}
         onChange={(e) => setInputData(e.target.value)}
+        customStyle={{ width: "20rem", height: "2rem" }}
       />
-      <S.Button onClick={() => onClick(editData)}>확인</S.Button>
+      <button onClick={() => onClick(editData)}>확인</button>
     </S.Container>
   );
 }
 
 const S = {
   Container: styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    ${FlexColumnCenterCss}
     width: 30rem;
     height: 15rem;
     gap: 10%;
     border-radius: 8px;
     font-size: 2rem;
-  `,
-  Input: styled.input`
-    width: 80%;
-    height: 15%;
-    border-radius: 1rem;
-    border: 2px solid black;
-  `,
-  Button: styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 1rem;
-    background-color: #d9d9d9;
-    width: 20%;
-    height: 13%;
-    font-size: 1rem;
+
+    & > button {
+      ${FlexCenterCSS}
+      border-radius: 1rem;
+      background-color: #d9d9d9;
+      width: 20%;
+      height: 13%;
+      font-size: 1rem;
+    }
   `,
 };
 

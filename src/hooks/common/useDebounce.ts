de76b1fee1faa9e2b8b51f0bfa83b2bc7653b609
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-12-08 16:28:31 +0900

    feat: useDebounce 구현

---
+++ src/hooks/common/useDebounce.ts
@@ -0,0 +1,16 @@
+import { useRef } from "react";
+
+const useDebounce = (cb: () => void, ms: number): (() => void) => {
+  const timer = useRef<ReturnType<typeof setTimeout>>();
+
+  const dispatchDebounce = () => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+    }
+    const newTimer = setTimeout(cb, ms);
+    timer.current = newTimer;
+  };
+  return dispatchDebounce;
+};
+
+export default useDebounce;

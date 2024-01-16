6c64beab0276c4b1ceb3ad850096e7f4c8f4c423
se9981 <se9981@naver.com>
2024-01-16 22:06:23 +0900

    chore: EmptySpace 컴포넌트화 작업

--- src/pages/MessagePage/index.tsx
+++ src/pages/MessagePage/index.tsx
@@ -6,7 +6,9 @@ import { getAvatar, getChatList } from "../../services/API";
 import UserCard from "./Components/UserCard/UserCard";
 import { getParticipation } from "../../utils/messageUtils";
 import { useParams } from "react-router-dom";
+import EmptySpace from "../../components/Empty/EmptySpace";
 import Room from "./Components/Room/Room";
+
 export interface DataProps {
   num: number;
   result: ProfileProps[];
@@ -51,13 +53,7 @@ const MessagePage = () => {
       
       {targetId && chatList ? (
         <Room roomList={chatList} targetId={targetId} />
-      ) : <S.EmptySpace>
-        <S.ImageContainer>
-          <img width="600px" src="/img/bottle.png" alt="bottle"/>
-        </S.ImageContainer>
-        <S.MyText>내 메시지</S.MyText>
-        <S.Text>친구나 그룹에 비공개 사진과 메시지를 보내보세요</S.Text>
-        </S.EmptySpace>
+      ) : <EmptySpace Mytext="내 메시지" text="친구나 그룹에 비공개 사진과 메시지를 보내보세요"/> 
         }
         
     </S.Container>
@@ -69,28 +65,6 @@ const S = {
     width: 100%;
     height: 100%;
   `,
-  EmptySpace: styled.div`
-    text-align: center;
-    padding: 20px;
-    `,
-    ImageContainer: styled.div`
-      margin-top: 100px;
-    `,
-    MyText: styled.p`
-      font-size: 40px;
-      padding-bottom:20px;
-      color: #333;
-    `,
-    Text: styled.p`
-      font-size: 25px;
-      padding:10px;
-      color: grey;
-    `,
-  // EmptySpace: styled.div`
-  //   margin-left: 300px;
-  //   height: 100vh;
-  //   background: url("/img/bottle.png") no-repeat center center;
-  // `,
 };
 
 export default MessagePage;
